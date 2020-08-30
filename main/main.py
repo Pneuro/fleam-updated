8be7c6aac59419e0e16ca61b76324274b5eacf85
Pneuro <57775304+Pneuro@users.noreply.github.com>
2020-08-30 10:56:42 -0400

    data retrieved successfully

--- main/main.py
+++ main/main.py
@@ -13,8 +13,8 @@ main = Blueprint('main', __name__,
 class ScrapeControl():
     def get_data():
         '''This runs the subprocess to actuate the scraper. '''
-        subprocess.run(['scrapy', 'crawl', 'fleam'])
-        return
+        subprocess.run(['scrapy', 'crawl', 'fleam_spider'])
+        return 'ran'
     
     
 @main.route('/')
@@ -38,11 +38,94 @@ def index():
 @main.route('/result')
 def result():
     
+    db_id = db.session.query(SearchQuery.id).order_by(SearchQuery.id.desc()).first()
+    db_query = db.session.query(SearchQuery.query).order_by(SearchQuery.id.desc()).first()
+    db_city = db.session.query(SearchQuery.city).order_by(SearchQuery.id.desc()).first()
+    db_price = db.session.query(SearchQuery.price).order_by(SearchQuery.id.desc()).first()
     
-    
-    dbq = db.session.query(SearchQuery.city).first()
-    
-    return f'{dbq}'
+    ScrapeControl.get_data()
+    return f'{db_id} {db_query} {db_city} {db_price}'
 
 
 # TODO Take most recent DB entry and run scraper
+
+
+# DBQ Methods
+# 'add_column', 
+# 'add_columns', 
+# 'add_entity', 
+# 'all', 
+# 'as_scalar', 
+# 'autoflush', 
+# 'column_descriptions', 
+# 'correlate', 
+# 'count', 
+# 'cte', 
+# 'delete', 
+# 'dispatch', 
+# 'distinct', 
+# 'enable_assertions', 
+# 'enable_eagerloads', 
+# 'except_', 
+# 'except_all',
+# 'execution_options', 
+# 'exists', 
+# 'filter', 
+# 'filter_by', 
+# 'first', 
+# 'first_or_404', 
+# 'from_self', 
+# 'from_statement', 
+# 'get', 
+# 'get_execution_options', 
+# 'get_or_404', 
+# 'group_by', 
+# 'having', 
+# 'instances', 
+# 'intersect', 
+# 'intersect_all', 
+# 'is_single_entity', 
+# 'join', 
+# 'label', 
+# 'lazy_loaded_from', 
+# 'limit', 
+# 'logger', 
+# 'merge_result', 
+# 'offset', 
+# 'one', 
+# 'one_or_none', 
+# 'only_return_tuples', 
+# 'options', 
+# 'order_by', 
+# 'outerjoin', 
+# 'paginate', 
+# 'params', 
+# 'populate_existing', 
+# 'prefix_with', 
+# 'reset_joinpoint', 
+# 'scalar', 
+# 'select_entity_from', 
+# 'select_from', 
+# 'selectable', 
+# 'session', 
+# 'slice', 
+# 'statement', 
+# 'subquery', 
+# 'suffix_with', 
+# 'union', 
+# 'union_all', 
+# 'update', 
+# 'value', 
+# 'values', 
+# 'whereclause', 
+# 'with_entities', 
+# 'with_for_update', 
+# 'with_hint', 
+# 'with_labels', 
+# 'with_lockmode', 
+# 'with_parent', 
+# 'with_polymorphic', 
+# 'with_session', 
+# 'with_statement_hint', 
+# 'with_transformation', 
+# 'yield_per'
