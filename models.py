854465c37e66170d87c4dab7794e5a1ce72d3d67
Pneuro <57775304+Pneuro@users.noreply.github.com>
2020-10-10 13:10:12 -0400

    updates to selections

--- models.py
+++ models.py
@@ -98,9 +98,104 @@ class SearchForm(Form):
         ('pueblo', 'pueblo, Colorado'),
         ('westslope', 'westernslope, Colorado'),
 
+        # Connecticut
+        ('newlondon', 'Eastern Connecticut'),
+        ('hartford', 'Hartford, Connecticut'),
+        ('newhaven', 'New Haven, Connecticut'),
+        ('nwct', 'Northwest Connecticut'),
+
+        # Delaware
+        ('delaware', 'Delaware'),
+
+        # District of Columbia
+        ('washingtondc', 'Washington DC')
+
+        # Florida
+        ('miami', 'Broward County, Florida'),
+        ('daytona', 'Daytona Beach, Florida'),
+        ('keys', 'Florida Keys, Florida'),
+        ('miami', 'Fort Lauderdale, Florida'),
+        ('fortmyers', 'ft myers / SW florida, Florida'),
+        ('gainesville', 'Gainesville, Florida'),
+        ('cfl', 'Heartland, Florida'),
+        ('jacksonville', 'Jacksonville, Florida'),
+        ('lakeland', 'Lakeland, Florida'),
+        ('miami', 'Miami / Dade, Florida'),
+        ('lakecity', 'North Central, Florida'),
+        ('ocala', 'Ocala, Florida'),
+        ('okaloosa', 'Okaloosa / Walton, Florida'),
+        ('orlando', 'Orlando, Florida'),
+        ('panamacity', 'Panama City, Florida'),
+        ('pensacola', 'Pensacola, Florida'),
+        ('sarasota', 'Sarasota-Bradenton, Florida'),
+        ('miami', 'South Florida, Florida'),
+        ('spacecoast', 'Space Coast, Florida'),
+        ('staugustine', 'St Augustine, Florida'),
+        ('tallahassee', 'Tallahassee, Florida'),
+        ('tampa', 'Tampa Bay Area, Florida'),
+        ('treasure', 'Treasure Coast, Florida'),
+        ('miami', 'Palm Beach County, Florida'),
+
+        # Georgia
+        ('albanyga', 'Albany, Georgia'),
+        ('athensga', 'Athens, Georgia'),
+        ('atlanta', 'Atlanta, Georgia'),
+        ('augusta', 'Augusta, Georgia'),
+        ('brunswick', 'Brunswick, Georgia'),
+        ('columbusga', 'Columbus, Georgia'),
+        ('macon', 'Macon / Warner Robins, Georgia'),
+        ('nwga', 'Northwest, Georgia'),
+        ('savannah', 'Savannah / Hinesville, Georgia'),
+        ('statesboro', 'Statesboro, Georgia'),
+        ('valdosta', 'Valdosta, Georgia'),
+
+        # Hawaii
+        ('honolulu', 'Hawaii'),
+
+        # Idaho
+        ('boise', 'Boise, Idaho'),
+        ('eastidaho', 'East Idaho'),
+        ('lewiston', 'Lewiston / Clarkston, Idaho'),
+        ('twinfalls', 'Twin Falls, Idaho'),
+
+        # Illinois
+        ('bn', 'Bloomington-Normal'),
+        ('champana', 'Champaign Urbana, Illinois'),
+        ('chicago', 'Chicago, Illinois'),
+        ('decatur', 'Decatur, Illinois'),
+        ('lasalle', 'La Salle Co, Illinois'),
+        ('mattoon', 'Mattoon-Charleston, Illinois'),
+        ('peoria', 'Peoria, Illinois'),
+        ('ci', 'Rockford, Illinois'),
+        ('ci', 'Southern Illinois'),
+        ('ci', 'Springfield, Illinois'),
+        ('ci', 'Wester Illinois'),
+
+        # Indiana
+        # Iowa
+        # Kansas
+        # Kentucy
+        # Louisiana
+        # Maine
+        # Maryland
+        # Massachusetts
+        # Michigan
+        # Minnesota
+        # Mississippi
+        # Missouri
+        # Montana
+        # Nebraska
+        # Nevada
+        # New Hampshire
+        # New Jersey
+        # New Mexico
+        # New  York
+        # North Carolina
+        # North Dakota
 
         # Ohio
         ('cleveland', 'Cleveland, Ohio'),
+
 
     ])
     price = IntegerField(default=500)
