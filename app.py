4252bb0141316c36dc1a6216b5e4ad2eeb90f432
Pneuro <57775304+Pneuro@users.noreply.github.com>
2020-10-11 10:52:07 -0400

    ahhh

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from db import db, ma
 from flask_migrate import Migrate
 import os
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hwpahxlqhtzioo:4a7d6c43785c6d5f551ea5f0ef3d6157b85655c127aeeda8376a09e1050d845e@ec2-54-224-124-241.compute-1.amazonaws.com:5432/dlfvt18p3ebom'
